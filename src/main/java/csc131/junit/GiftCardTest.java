6a42d7b7c247f9c1b18f888ed72360bbbdadd49c
dell <ttaarriik@mail.ru>
2020-11-20 16:19:44 -0800

    Final changes

--- src/main/java/csc131/junit/GiftCardTest.java
+++ src/main/java/csc131/junit/GiftCardTest.java
@@ -36,10 +36,10 @@ public class GiftCardTest
 	@Test
 	public void deductBalance()
 	{
-	     double       balance;        
-	     GiftCard     card;
-	     int          issuingStore;
-	     String       s;
+	     double balance;        
+	     GiftCard card;
+	     int issuingStore;
+	     String s;
 	        
 	        
 	     issuingStore = 1337;
@@ -48,5 +48,57 @@ public class GiftCardTest
 	     s = "Remaining Balance: " + String.format("%6.2f", 80.00);
 	        assertEquals("deduct(20.00)", 
 	                     s, card.deduct(20.0));
+	    }
+	
+	 @Test
+	 public void deductAmountDue()
+	 {
+	     double balance;        
+	     GiftCard card;
+	     int issuingStore;
+	     String s;
+	        
+	        
+	     issuingStore = 1337;
+	     balance      = 100.00;
+	     card = new GiftCard(issuingStore, balance);
+	     s = "Amount Due: " + String.format("%6.2f", 10.00);
+	     assertEquals("deduct 110.00 from 100.00", 
+	                     s, card.deduct(110.0));
+	  }
+
+	 @Test
+	 public void deductInvalidTransaction()
+	 {
+	     double balance;        
+	     GiftCard card;
+	     int issuingStore;
+	     String s;
+	        
+	        
+	     issuingStore = 1337;
+	     balance      = 100.00;
+	     card = new GiftCard(issuingStore, balance);
+	     s = "Invalid Transaction";
+	     assertEquals("deduct -10.00 from 100.00", 
+	                     s, card.deduct(-10.0));
+	    }
+	 
+	 @Test(expected = IllegalArgumentException.class)
+	    public void constructor_IncorrectBalance() throws IllegalArgumentException
+	    {
+	        new GiftCard(1, -100.00);
+	    }
+	 
+	 @Test(expected = IllegalArgumentException.class)
+	    public void constructor_IncorrectID_Low()
+	    {
+	        new GiftCard(-1, 100.00);
+	    }
+	 
+	 @Test(expected = IllegalArgumentException.class)
+	    public void constructor_IncorrectID_High()
+	    {
+	        new GiftCard(100000, 100.00);
 	    }
 }
